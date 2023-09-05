cac502278dab6f2922b3737dc0490dcc45b92a55
00wz <130168402+00wz@users.noreply.github.com>
2023-09-05 15:05:24 +0300

    lesson5classwork

--- Assets/Scripts/PlayFabAccountManager.cs
+++ Assets/Scripts/PlayFabAccountManager.cs
@@ -1,6 +1,7 @@
 using PlayFab;
 using PlayFab.ClientModels;
 using System;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
@@ -12,6 +13,28 @@ public class PlayFabAccountManager : MonoBehaviour
     private void Start()
     {
         PlayFabClientAPI.GetAccountInfo(new GetAccountInfoRequest(), OnGetAccount, OnError);
+        PlayFabClientAPI.GetCatalogItems(new GetCatalogItemsRequest(), OnGetCatalogSuccess, OnError);
+        PlayFabServerAPI.GetRandomResultTables(new PlayFab.ServerModels.GetRandomResultTablesRequest(), 
+            OnGetTableSuccess, OnError);
+    }
+
+    private void OnGetTableSuccess(PlayFab.ServerModels.GetRandomResultTablesResult result)
+    {
+        
+    }
+
+    private void OnGetCatalogSuccess(GetCatalogItemsResult result)
+    {
+        Debug.Log("OnGetCatalogSuccess");
+        ShowItems(result.Catalog);
+    }
+
+    private void ShowItems(List<CatalogItem> catalog)
+    {
+        foreach(var item in catalog)
+        {
+            Debug.Log($"{item.ItemId}");
+        }
     }
 
     private void OnError(PlayFabError error)
